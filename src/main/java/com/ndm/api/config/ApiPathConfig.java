6260237927f09475cf756db300f3178f1ebd352b
dckhoa203 <khoadochi1998@gmail.com>
2022-04-07 19:48:36 +0700

    Factory code

--- src/main/java/com/ndm/api/config/ApiPathConfig.java
+++ src/main/java/com/ndm/api/config/ApiPathConfig.java
@@ -18,7 +18,7 @@ public class ApiPathConfig {
 
     public static final String GET_ALL_NTP_SERVER_BY_DEVICE_ID_URL = VERSION + "{deviceId}/ntpServers";
     public static final String ADD_NTP_SERVER_TO_DEVICE_URL = VERSION + "ntpServer";
-    public static final String UPDATE_NTP_SERVER_URL = VERSION + "ntpServer/{id}";
+    public static final String UPDATE_NTP_SERVER_URL = VERSION + "ntpServer";
 
     public static final String GET_ALL_DEVICE_URL = VERSION + "devices";
     public static final String GET_DEVICE_BY_ID_URL = VERSION + "device/{id}";
